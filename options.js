d108e98b44e35441eabd1727a552860191e7d952
ademdeojee <54884928+ademdeojee@users.noreply.github.com>
2020-10-15 16:26:58 +1100

    automatic changing development

--- options.js
+++ options.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     restoreOptions();
     var link = document.querySelector("#days");
     link.onchange = function (){
-        chrome.storage.sync.set({'nextDay' : link.value});
+        chrome.storage.sync.set({'nextDay' : link.value}, function(){console.log("value changed to " + link.value);});
         restoreOptions();
     };
 });
--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+    console.log("Got an alarm!", alarm);
+    chrome.storage.sync.get({
+        checkState: false
+    }, function (items) {
+        chrome.storage.sync.set({ 'checkState': !items.checkState });
+        restoreOptions();
+    });
+    chrome.storage.sync.get('nextDay', function (items) {
+        //chrome.alarms.create('flip', {when : getNextDayOfWeek(items.nextDay).valueOf() });
+        chrome.alarms.create('flip', {delayInMinutes: 1});
+    });
+});
+
+chrome.storage.sync.get('nextDay', function (items) {
+    //chrome.alarms.create('flip', {when : getNextDayOfWeek(items.nextDay).valueOf() });
+    chrome.alarms.create('flip', {delayInMinutes: 1});
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     restoreOptions();
@@ -8,10 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
 function checkValue() {
     var checkBox = document.querySelector("#check");
     var text = document.querySelector(".bin-info");
-
     chrome.storage.sync.set({ 'checkState': checkBox.checked });
-
-    if (checkBox.checked == true) {
+    if (checkBox.checked) {
         text.innerHTML = "Green";
         text.style.color = "#388e3c";
     } else {
@@ -27,11 +43,16 @@ function restoreOptions() {
         document.querySelector("#check").checked = items.checkState;
         checkValue();
     });
+    chrome.storage.sync.get('nextDay', function (items) {
+        document.querySelector(".date").innerHTML = "The next " + getNextDayOfWeek(items.nextDay).toLocaleString('en-us', {  weekday: 'long' }) + " is " + getNextDayOfWeek(items.nextDay).toLocaleString('en-us', {  month: 'long', day :'numeric'});
+    });
 }
 
 function getNextDayOfWeek(dayOfWeek) {
-    //returns today if today is day of week
+    //returns next day of week if today is day of week
     var d = new Date();
-    d.setDate(d.getDate() + ((7-d.getDay())%7+dayOfWeek) % 7);
+    d.setDate(d.getDate() + (7-d.getDay())%7+dayOfWeek);
     return d;
 }
+
+
