1b7e710e1455e14ec03d102199b786195061cebd
ademdeojee <adem.deojee@student.uts.edu.au>
2020-10-19 18:09:35 +1100

    version 1.0 complete

--- options.js
+++ options.js
@@ -1,14 +1,13 @@
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     restoreOptions();
     var link = document.querySelector("#check");
-    link.addEventListener('click', function(){
+    link.addEventListener('click', function() {
         var checkBox = document.querySelector("#check");
         chrome.storage.sync.set({ 'checkState': checkBox.checked });
     });
     var link = document.querySelector("#days");
-    link.onchange = function (){
-        chrome.storage.sync.set({'nextDay' : link.value});
+    link.onchange = function() {
+        chrome.storage.sync.set({ 'nextDay': link.value });
         restoreOptions();
     };
 });
@@ -16,14 +15,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function restoreOptions() {
-    chrome.storage.sync.get('nextDay', function (items) {
+    chrome.storage.sync.get('nextDay', function(items) {
         document.querySelector("#days").value = items.nextDay;
         chrome.alarms.clearAll();
-        chrome.alarms.create('flip', {when : getNextDayOfWeek(items.nextDay).valueOf() });
+        chrome.alarms.create('flip', { when: getNextDayOfWeek(items.nextDay).valueOf() });
+    });
+    chrome.storage.sync.get('checkState', function(items) {
+        var link = document.querySelector("#check");
+        link.checked = items.checkState;
     });
 }
 
-function getNextDayOfWeek(dayOfWeek){
+function getNextDayOfWeek(dayOfWeek) {
     var d = new Date();
     d.setDate(d.getDate() + (dayOfWeek - 1 - d.getDay() + 7) % 7 + 1);
     d.setHours(23, 59, 58);
--- popup.js
+++ popup.js
@@ -1,29 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var text = document.querySelector("#bin-info");
-    chrome.storage.sync.get('checkState', function (items) {
+    var image = document.querySelector('#bin');
+    chrome.storage.sync.get('checkState', function(items) {
         if (items.checkState) {
             text.innerHTML = "Green";
-            text.style.color = "#388e3c";
+            text.style.color = "#00ac54";
+            image.src = 'GreenBin.png';
         } else {
             text.innerHTML = "Yellow";
-            text.style.color = "#fbc02d";
+            text.style.color = "#ffc622";
+            image.src = 'YellowBin.png';
         }
     });
-    chrome.storage.sync.get('nextDay', function (items) {
+    chrome.storage.sync.get('nextDay', function(items) {
         var now = new Date();
-        if(getNextDayOfWeek(items.nextDay).getDate()-7 == now.getDate()){
+        if (getNextDayOfWeek(items.nextDay).getDate() - 7 == now.getDate()) {
             document.querySelector("#date").innerHTML = "Today Is Bin Day (" + now.toLocaleString('en-au', { weekday: 'short', day: 'numeric', month: 'numeric' }) + ")";
-        }else{
-            document.querySelector("#date").innerHTML = "The Next Bin Day: " + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { day: 'numeric', month: 'numeric' }) + " (" + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { weekday: 'short' }) + ")"; 
+        } else {
+            document.querySelector("#date").innerHTML = "The Next Bin Day: " + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { day: 'numeric', month: 'numeric' }) + " (" + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { weekday: 'short' }) + ")";
         }
+    });
+    var link = document.querySelector("#settings");
+    link.addEventListener('click', function() {
+        var win = window.open("chrome-extension://glipffkfpoiblabcalcgeainmbpjhkfo/options.html", '_blank');
+        win.focus();
     });
 });
 
-function getNextDayOfWeek(dayOfWeek){
+function getNextDayOfWeek(dayOfWeek) {
     var d = new Date();
     d.setDate(d.getDate() + (dayOfWeek - 1 - d.getDay() + 7) % 7 + 1);
     d.setHours(23, 59, 58);
     return d;
 };
-
-
