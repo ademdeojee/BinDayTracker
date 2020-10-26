46cd3aa99f99bd9a2edeeb77f468de04cc52582b
ademdeojee <54884928+ademdeojee@users.noreply.github.com>
2020-10-27 10:56:36 +1100

    Revert "fix in the works"
    This reverts commit eeb1649eb93486b23db51dfdc4741d37b5091027.

--- popup.js
+++ popup.js
@@ -1,17 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var text = document.querySelector("#bin-info");
     var image = document.querySelector('#bin');
-    var defaultdate = getNextDayOfWeek(1);
-    chrome.storage.sync.get({'nextDay' : defaultdate}, function(items) {
-        console.log(items.nextDay);
-        var now = new Date();
-        if(now > items.nextDay){
-            chrome.storage.sync.set({ 'nextDay': getNextDayOfWeek(items.nextDay.getDay()) });
-            chrome.storage.sync.get('checkState', function (items) {
-                chrome.storage.sync.set({ 'checkState': !items.checkState });
-            });
-        }
-    });
     chrome.storage.sync.get('checkState', function(items) {
         if (items.checkState) {
             text.innerHTML = "Green";
@@ -23,11 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
             image.src = 'YellowBin.png';
         }
     });
-    chrome.storage.sync.get({'nextDay' : defaultdate}, function(items) {
-        if (items.nextDay.getDate() - 7 == now.getDate()) {
+    chrome.storage.sync.get({'nextDay' : '1'}, function(items) {
+        var now = new Date();
+        if (getNextDayOfWeek(items.nextDay).getDate() - 7 == now.getDate()) {
             document.querySelector("#date").innerHTML = "Today Is Bin Day (" + now.toLocaleString('en-au', { weekday: 'short', day: 'numeric', month: 'numeric' }) + ")";
         } else {
-            document.querySelector("#date").innerHTML = "The Next Bin Day: " + items.nextDay.toLocaleString('en-au', { day: 'numeric', month: 'numeric' }) + " (" + items.nextDay.toLocaleString('en-au', { weekday: 'short' }) + ")";
+            document.querySelector("#date").innerHTML = "The Next Bin Day: " + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { day: 'numeric', month: 'numeric' }) + " (" + getNextDayOfWeek(items.nextDay).toLocaleString('en-au', { weekday: 'short' }) + ")";
         }
     });
 });
